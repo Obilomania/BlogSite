c86ea8ac2a4a4b177dfada683db43fdc27abdc93
Obilomania <iloanyaobinna@gmail.com>
2023-02-23 14:27:37 +0100

    Made a blogPost section and implemented its css

---
+++ BlogSite/Areas/User/Controllers/MainBlogController.cs
@@ -0,0 +1,24 @@
+﻿using BlogSite.DataAccess.Repository.IRepository;
+using BlogSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.Areas.User.Controllers
+{
+    [Area("User")]
+    public class MainBlogController : Controller
+    {
+        private readonly IBlogPostRepository _context;
+
+        public MainBlogController(IBlogPostRepository context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<BlogPost> posts = await _context.GetAll();
+
+            return View(posts);
+        }
+    }
+}
